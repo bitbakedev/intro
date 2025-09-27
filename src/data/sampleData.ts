0f24c6c0354e1810fbd2e7878604e59f2fd7bb76
bitbakedev <coinbakenow@gmail.com>
2025-09-28 00:29:36 +0900

    Updated sampleData.ts

--- src/data/sampleData.ts
+++ src/data/sampleData.ts
@@ -216,7 +216,7 @@ export const sampleProposalData: ProposalData = {
       position: "",
       education: "",
       experience: "• 요기요 - 서비스 기획자\n• 주식회사 핀크 (Finnq) - 서비스 기획자\n• 유플리트 - UX 기획자\n• Inition Inc. - UX Designer",
-      linkedin: "https://www.linkedin.com/in/ahyeon921/"
+      linkedin: "https://www.linkedin.com/in/%ED%98%81%EC%B0%BD-%EA%B6%8C-877893243/"
     },
   ],
   
