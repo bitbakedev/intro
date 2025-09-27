0159404eddf9fabd44748af19b8840a1f64f3f7f
bitbakedev <coinbakenow@gmail.com>
2025-09-27 21:35:56 +0900

    Updated Solution.tsx

--- src/components/sections/Solution.tsx
+++ src/components/sections/Solution.tsx
@@ -118,6 +118,9 @@ const Solution: React.FC = () => {
 
       
       <h2 className="text-2xl font-bold text-gray-800 mb-6">4. 지속적인 온램프 솔루션 탐색</h2>
+      <p className="text-gray-700 leading-relaxed mb-4">
+        법정화폐를 암호화폐로 교환하는 수단을 '온램프 솔루션' 이라고 합니다.
+      </p>
       
       <p className="text-gray-700 leading-relaxed mb-4">
         한국은 약 8조 원 규모의 거대한 기프트카드 시장을 보유하고 있습니다. 비트베이크는 이 시장 특성을 활용해, 
