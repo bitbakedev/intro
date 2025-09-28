974b509901348c30145faf1b19d4518478e56392
bitbakedev <coinbakenow@gmail.com>
2025-09-28 17:00:31 +0900

    Updated ExitStrategy.tsx

--- src/components/sections/ExitStrategy.tsx
+++ src/components/sections/ExitStrategy.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ExitStrategy: React.FC = () => {
   return (
     <div className="prose prose-lg max-w-none">
-      <p className="mb-5">비트베이크는 Seed 투자 유치 후 1년 이내에 뚜렷한 성과를 만들어 Series A 투자 유치를 진행할 계획입니다.
+      <p className="mb-4">비트베이크는 Seed 투자 유치 후 1년 이내에 뚜렷한 성과를 만들어 Series A 투자 유치를 진행할 계획입니다.
 이 과정에서 기존 투자자는 Series A 진입 시 신규 투자자에게 구주 매각을 통해 Exit(투자금 회수)이 가능합니다.</p>
       <p className="mb-5">
       Series A 라운드는 Neo 블록체인 재단을 리드 투자사로 유치하는 것을 목표로 하고 있으며,
